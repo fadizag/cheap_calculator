81a3ec31c0e333785cc50e2ef2db0de4b54d490c
fadizag <fadizagharneh@gmail.com>
2026-09-24 11:11:32 +0300

    Refresh PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,14 @@
-const CACHE = "sheep-calculator-v1";
+const CACHE = "sheep-calculator-v3";
 const ASSETS = ["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg"];
+
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
+
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
+
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
